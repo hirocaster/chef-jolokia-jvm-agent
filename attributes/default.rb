9778dce2ea227baf62149899f897aef702dd807c
hirocaster <hohtsuka@gmail.com>
2015-01-08 22:13:36 +0900

    Initial commit

---
+++ attributes/default.rb
@@ -0,0 +1,2 @@
+default['jolokia-jvm-agent']['version'] = "1.2.3"
+default['jolokia-jvm-agent']['dir'] = "/opt/jolokia"
---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'jolokia-jvm-agent'
+maintainer       'hirocaster'
+maintainer_email 'hohtsuka@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures jolokia-jvm-agent'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
