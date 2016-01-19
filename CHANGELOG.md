31e772e31f28d6bfa3e3e810d2d2ba914ddd8a60
E. Camden Fisher <fish@fishnix.net>
2016-01-19 15:27:00 -0500

    Add tests and boilerplate chef stuf

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,11 @@ This file is used to list changes made in each version of the jolokia-jvm-agent 
 -----
 - [hirocaster] - Initial release of jolokia-jvm-agent
 
+0.1.1
+-----
+- [fishnix] - Add basic unit and integration tests
+- [fishnix] - Minor style fixes, add Berksfile, .gitignore
+
 - - -
 Check the [Markdown Syntax Guide](http://daringfireball.net/projects/markdown/syntax) for help with Markdown.
 
---
+++ spec/unit/recipes/default_spec.rb
@@ -0,0 +1,38 @@
+#
+# Cookbook Name:: jolokia-jvm-agent
+# Spec:: default
+#
+require 'spec_helper'
+
+describe 'jolokia-jvm-agent::default' do
+  platforms = {
+    'centos' => ['6.6', '7.0'],
+    'ubuntu' => ['14.04']
+  }
+
+  platforms.each do |platform, versions|
+    versions.each do |version|
+      context "on #{platform.capitalize} #{version}" do
+        let(:chef_run) do
+          ChefSpec::SoloRunner.new(log_level: :error, platform: platform, version: version) do |node|
+            node.set['jolokia-jvm-agent']['version'] = '1.2.3'
+            node.set['jolokia-jvm-agent']['dir'] = '/opt/jolokia'
+          end.converge(described_recipe)
+        end
+
+        it 'should create the default jolokia-jvm-agent installation directory' do
+          expect(chef_run).to create_directory('/opt/jolokia').with(owner: 'root')
+        end
+
+        it 'creates the jolokia-jvm-agent jar file' do
+          expect(chef_run).to create_remote_file('/opt/jolokia/jolokia-jvm-1.2.3-agent.jar').with(owner: 'root')
+        end
+
+        it 'creates a link to the jolokia-jvm-agent' do
+          expect(chef_run).to create_link('/opt/jolokia/jolokia-jvm-agent.jar').with(
+            to: '/opt/jolokia/jolokia-jvm-1.2.3-agent.jar')
+        end
+      end
+    end
+  end
+end
