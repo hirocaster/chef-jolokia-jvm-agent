7c6fd97d09ec6bf4bb20b25f41f426818795b1f3
hirocaster <hohtsuka@gmail.com>
2016-01-24 01:19:11 +0900

    Add metadata
    - Add issues url
    - Add supports os

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,8 @@ description      'Installs/Configures jolokia-jvm-agent'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.0'
 source_url       'https://github.com/hirocaster/chef-jolokia-jvm-agent'
+issues_url       'https://github.com/hirocaster/chef-jolokia-jvm-agent/issues'
+
+%w{centos ubuntu}.each do |os|
+  supports os
+end
